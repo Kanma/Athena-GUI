c33464cd3535e004d0aa99b4b6648905e3b847ed
Philip Abbet <philip.abbet@gmail.com>
2013-03-08 09:27:16 +0100

    Creation of the library

---
+++ include/Athena-GUI/Prerequisites.h
@@ -0,0 +1,41 @@
+/** @file   Prerequisites.h
+    @author Philip Abbet
+
+    Declaration of the types of the Athena-GUI module
+*/
+
+#ifndef _ATHENA_GUI_PREREQUISITES_H_
+#define _ATHENA_GUI_PREREQUISITES_H_
+
+#include <Athena-Core/Prerequisites.h>
+#include <Athena-GUI/Config.h>
+
+
+/// Used to export symbols from the library
+#if (ATHENA_PLATFORM == ATHENA_PLATFORM_WIN32)
+#    ifdef ATHENA_GUI_EXPORTS
+#        define ATHENA_GUI_SYMBOL __declspec(dllexport)
+#    else
+#        define ATHENA_GUI_SYMBOL __declspec(dllimport)
+#    endif
+#else
+#    define ATHENA_GUI_SYMBOL
+#endif
+
+
+//---------------------------------------------------------------------------------------
+/// @brief  Main namespace. All the components of the Athena engine belongs to this
+///         namespace
+//---------------------------------------------------------------------------------------
+namespace Athena
+{
+    //-----------------------------------------------------------------------------------
+    /// @brief  Contains all the GUI-related classes
+    //-----------------------------------------------------------------------------------
+    namespace GUI
+    {
+        ATHENA_GUI_SYMBOL extern const char* VERSION;
+    }
+}
+
+#endif
